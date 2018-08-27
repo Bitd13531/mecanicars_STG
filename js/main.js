bd468aa0aedcd2331d91d8e51c1ce13e02aeadc1
Angel Villasmil <anvill_2002@hotmail.com>
2018-08-27 01:25:44 -0300

    cambios en el index.html, media querys 640 a 768 lista y main js menu

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-let menuVisible = false;
+let menuVisible = false
 let qlick = document.getElementById('menu__bar')
 let mb = document.querySelector('nav')
-if (qlick && mb){
+const rwd = matchMedia('(max-width: 770px)')
 
-qlick.addEventListener('click', e =>{
-        
-    if (!menuVisible){
+if (qlick && mb) {
+  qlick.addEventListener('click', e => {
+    if (rwd.matches) {
+      if (!menuVisible) {
         mb.style.transform = 'translateX(0%)'
         mb.style.transition = '0.5s'
-        
-        menuVisible = true;
-    }else {
+
+        menuVisible = true
+      } else {
         mb.style.transform = 'translateX(100%)'
         mb.style.transition = '0.5s'
-        
-        menuVisible = false;
+
+        menuVisible = false
+      }
     }
-    
-})
+  })
 }
